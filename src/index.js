b92617ae66af3f63b7cca27b38e31f4d1d0784fe
Carlos Ferreira <carlosferreira@outlook.com>
2017-06-24 12:32:44 +0100

    First working version to get bitcoin value

--- src/index.js
+++ src/index.js
@@ -1,69 +1,72 @@
 'use strict';
 var Alexa = require('alexa-sdk');
+var https = require('https');
 
-//=========================================================================================================================================
-//TODO: The items below this comment need your attention.
-//=========================================================================================================================================
-
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
-//Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
 var APP_ID = undefined;
-
-var SKILL_NAME = "Space Facts";
-var GET_FACT_MESSAGE = "Here's your fact: ";
-var HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
-var HELP_REPROMPT = "What can I help you with?";
+var SKILL_NAME = "cBot";
 var STOP_MESSAGE = "Goodbye!";
 
-//=========================================================================================================================================
-//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
-//=========================================================================================================================================
-var data = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
-];
+var CBOT_SAY = 'seebot';
 
-//=========================================================================================================================================
-//Editing anything below this line might break your skill.  
-//=========================================================================================================================================
-exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+exports.handler = function(event, context, callback)
+{
+	var alexa = Alexa.handler(event, context);
+	alexa.APP_ID = APP_ID;
+	alexa.registerHandlers(handlers);
+	alexa.execute();
 };
 
 var handlers = {
-    'LaunchRequest': function () {
-        this.emit('GetNewFactIntent');
-    },
-    'GetNewFactIntent': function () {
-        var factArr = data;
-        var factIndex = Math.floor(Math.random() * factArr.length);
-        var randomFact = factArr[factIndex];
-        var speechOutput = GET_FACT_MESSAGE + randomFact;
-        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
-    },
-    'AMAZON.HelpIntent': function () {
-        var speechOutput = HELP_MESSAGE;
-        var reprompt = HELP_REPROMPT;
-        this.emit(':ask', speechOutput, reprompt);
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', STOP_MESSAGE);
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', STOP_MESSAGE);
-    }
+	'LaunchRequest': function()
+	{
+		this.emit('StartIntent');
+	},
+	'StartIntent': function()
+	{
+		var speechOutput = 'Hello there, I am ' + CBOT_SAY + ' how may I help?';
+		var reprompt = 'Do not fear ' + CBOT_SAY + ' is here!';
+		this.emit(':ask', speechOutput, reprompt);
+	},
+	'GetBitcoinValueIntent': function()
+	{
+		// var currency = this.event.request.intent.slots.currency;
+		var req = https.request({
+			host: 'carlos.fyi',
+			port: 443,
+			path: '/api/cbot.php?method=bitcoinValue',
+			method: 'GET'
+		},
+		res => {
+			res.setEncoding('utf8');
+
+			var returnData = '';
+			res.on('data', chunk => {
+				returnData = returnData + chunk;
+			});
+
+			res.on('end', () => {
+				var bitcoinValue = JSON.parse(returnData).values.USD;
+				var speechOutput = 'A bitcoin is worth ' + bitcoinValue + ' dollars';
+				var cardTitle = SKILL_NAME + ' Bitcoin Value';
+				this.emit(':tellWithCard', speechOutput, cardTitle, speechOutput);
+			});
+		});
+		req.end();
+	},
+	'AMAZON.HelpIntent': function ()
+	{
+		var HELP_MESSAGE = 'I am ' + CBOT_SAY + ' and I can help you with many things. I am also always learning new things!';
+		var HELP_REPROMPT = "What can I help you with?";
+		var speechOutput = HELP_MESSAGE;
+		var reprompt = HELP_REPROMPT;
+		this.emit(':ask', speechOutput, reprompt);
+	},
+	'AMAZON.CancelIntent': function ()
+	{
+		this.emit(':tell', STOP_MESSAGE);
+	},
+	'AMAZON.StopIntent': function ()
+	{
+		this.emit(':tell', STOP_MESSAGE);
+	}
 };
